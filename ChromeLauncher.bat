6f9e27dc40f8452a394ba32c7a074a047d948c16
PrabhashDiss <101409417+PrabhashDiss@users.noreply.github.com>
2023-01-27 16:33:43 +0530

    Update ChromeLauncher.bat

--- ChromeLauncher.bat
+++ ChromeLauncher.bat
@@ -1,4 +1,11 @@
-rem A simple script that will Open Google Chrome and Navigate to a specific Websites
+:: A simple script that will Open Google Chrome and Navigate to a specific Websites
 
 @echo off
-start chrome.exe "https://online.uom.lk/my/"
+echo Available Options:
+echo 1. Google
+echo 2. YouTube
+echo 3. Facebook
+set /p choice=Enter the option number of the website you want to navigate to: 
+if %choice%==1 start chrome.exe "https://www.google.com"
+if %choice%==2 start chrome.exe "https://www.youtube.com"
+if %choice%==3 start chrome.exe "https://www.facebook.com"
